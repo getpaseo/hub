fab90d7907f87ab09dc8db320895c2a7eb01e57e
Mohamed Boudra <boudra.moha@gmail.com>
2026-08-10 13:18:42 +0200

    chore(auth): format HTTP sign-in regression

--- src/root-head.test.tsx
+++ src/root-head.test.tsx
@@ -5,8 +5,5 @@ import { Route } from "./routes/__root.js";
 it("limits referrers to same-origin requests so CSRF checks work over HTTP", async () => {
   const head = await Route.options.head?.(undefined!);
 
-  assert.equal(
-    head?.meta?.find((meta) => meta?.name === "referrer")?.content,
-    "same-origin",
-  );
+  assert.equal(head?.meta?.find((meta) => meta?.name === "referrer")?.content, "same-origin");
 });
